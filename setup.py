8724203955103f0d2e49f1cfcadd5bf294334a08
angldaniel <angeldaniel@wisthub.com>
2025-02-13 10:33:09 -0500

    Mi primer release

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miTercerRepo",    # Nombre del paquete
+    version="0.1.0",    # Versión inicial
+    packages=find_packages(),    # Paquetes a incluir
+    description="Un paquete pip simple de saludo",    # Breve descripción
+    author="ANGEL LOPEZ",    # Tu nombre
+    author_email="angeldaniel@wisthub.net",    # Tu correo electrónico
+    url="https://github.com/angeldaniel-wh/miTercerRepo",    # URL del proyecto
+)
